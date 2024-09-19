71c4497f41b7453012d480718f6184ad412f8452
Timothy Riddle <tim.riddle.tr@gmail.com>
2024-09-19 15:08:37 -0600

    Edited the continue text, Added the game over collision for out of bounds and if the head of the snake makes contact with the body.  Added the body of the snake to follow the head.

--- Code/snake.py
+++ Code/snake.py
@@ -1,6 +1,7 @@
 import pygame, random
 from os.path import join
 
+
 #Initialize Pygame
 pygame.init()
 
@@ -48,7 +49,7 @@ gameover_text = font.render("GAMEOVER", True, RED, DARKGREEN)
 gameover_rect = gameover_text.get_rect()
 gameover_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
 
-continue_text = font.render("Press continue to play again", True, RED, DARKGREEN)
+continue_text = font.render("Press any key to play again", True, RED, DARKGREEN)
 continue_rect = continue_text.get_rect()
 continue_rect.center = (WINDOW_WIDTH / 2 , WINDOW_HEIGHT / 2 + 64)
 
@@ -90,12 +91,48 @@ while running:
         if event.key == pygame.K_DOWN:
             snake_dx = 0
             snake_dy = SNAKE_SIZE
+# 
 
+    #Add the head coordinate to the first index of the body coordinate list
+    #This will essentially move all of the snakes body boy one position in the list.
+    body_coords.insert(0, head_coord)
+    body_coords.pop()
+    
     #Update the x, y position of the snakes head and make a new coordinate
     head_x += snake_dx
     head_y += snake_dy
     head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
     
+    #Check for game over
+    if head_rect.left < 0 or head_rect.right > WINDOW_WIDTH or head_rect.top < 0 or head_rect.bottom > WINDOW_HEIGHT or head_coord in body_coords:
+        display_surface.blit(gameover_text, gameover_rect)
+        display_surface.blit(continue_text, continue_rect)
+        pygame.display.update()
+
+        #Pause the game until player presses a key, then reset the game
+        is_paused = True
+        while is_paused:
+            for event in pygame.event.get():
+                #player wants to play again
+                if event.type == pygame.KEYDOWN:
+                    score = 0
+
+                    head_x = WINDOW_WIDTH / 2
+                    head_y = WINDOW_HEIGHT / 2
+                    head_coord = (head_x, head_y, SNAKE_SIZE, SNAKE_SIZE)
+
+                    body_coords = []
+
+                    snake_dx = 0
+                    snake_dy = 0
+
+                    is_paused = False
+                #Player wants to quit.
+                if event.type == pygame.QUIT:
+                    is_paused = False
+                    running = False
+
+
     #Check for Collisions
     if head_rect.colliderect(apple_rect):
         score += 1
@@ -104,6 +141,8 @@ while running:
         apple_x = random.randint(0, WINDOW_WIDTH - SNAKE_SIZE)
         apple_y = random.randint(0, WINDOW_HEIGHT - SNAKE_SIZE)
         apple_coord = (apple_x, apple_y, SNAKE_SIZE, SNAKE_SIZE)
+
+        body_coords.append(head_coord)
 
     #Update HUD
     score_text =font.render(f"Score: {score}", True, GREEN, DARKRED)
@@ -117,8 +156,11 @@ while running:
     display_surface.blit(score_text, score_rect)
 
     #Display Assets
+    for body in body_coords:
+        pygame.draw.rect(display_surface, DARKGREEN, body)
     head_rect = pygame.draw.rect(display_surface, GREEN, head_coord)
     apple_rect = pygame.draw.rect(display_surface, RED, apple_coord)
+
 
     #Update display and tick clock
     pygame.display.update()
